b60f9c8bcbf534011d7e74a2321e0a2925232fb6
hyan150101 <202509057461@alunos.estacio.br>
2025-09-08 14:12:54 -0300

    Update supertrunfoNOVATO.c

--- supertrunfoNOVATO.c
+++ supertrunfoNOVATO.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+
+struct Carta {
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+};
+
+int main() {
+        struct Carta carta1, carta2;
+
+     printf("Cadastro da primeira carta:\n");
+    printf("Populacao: ");
+    scanf("%d", &carta1.populacao);
+    printf("Area: ");
+    scanf("%f", &carta1.area);
+    printf("PIB: ");
+    scanf("%f", &carta1.pib);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &carta1.pontos_turisticos);
+
+    printf("\n");
+
+    printf("Cadastro da segunda carta:\n");
+    printf("Populacao: ");
+    scanf("%d", &carta2.populacao);
+    printf("Area: ");
+    scanf("%f", &carta2.area);
+    printf("PIB: ");
+    scanf("%f", &carta2.pib);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &carta2.pontos_turisticos);
+
+    printf("\n--- Dados cadastrados ---\n");
+
+    printf("\nCarta 1:\n");
+    printf("Populacao: %d\n", carta1.populacao);
+    printf("Area: %.2f\n", carta1.area);
+    printf("PIB: %.2f\n", carta1.pib);
+    printf("Numero de pontos turisticos: %d\n", carta1.pontos_turisticos);
+
+      printf("\nCarta 2:\n");
+    printf("Populacao: %d\n", carta2.populacao);
+    printf("Area: %.2f\n", carta2.area);
+    printf("PIB: %.2f\n", carta2.pib);
+    printf("Numero de pontos turisticos: %d\n", carta2.pontos_turisticos);
+
+    return 0;
+}
 
